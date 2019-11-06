d2d9846df021c8906f8137f2dbc92b0c32c8aede
Patrick Barsallo <patrick@pullrequest.com>
2019-11-06 13:48:32 -0600

    remove another comment

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name', 'email', 'password',
     ];
-//asdf
+
     /**
      * The attributes that should be hidden for arrays.
      *
