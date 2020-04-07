4f07b232935485d94de874327b6541dc2810b391
Patrick Barsallo <patrick@pullrequest.com>
2020-04-07 09:42:16 -0500

    asdf

--- app/User.php
+++ app/User.php
@@ -4,10 +4,9 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-
 class User extends Authenticatable
 {
-//    use Notifiable;
+    use Notifiable;
 // 1
 
   
