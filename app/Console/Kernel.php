c24d67a2c145abc88e33900f6c49313c0958ff70
Patrick Barsallo <patrick@pullrequest.com>
2021-12-06 10:03:34 -0600

    test commit

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,6 +5,8 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
+// another commit
+
 class Kernel extends ConsoleKernel
 {
     /**
