d87a3d8a23d76a4b458ffaa748af05538fbaf8c3
Patrick Barsallo <patrick@pullrequest.com>
2023-01-30 08:31:09 -0600

    frontend

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -20,3 +20,18 @@ Vue.component('example', require('./components/Example.vue'));
 const app = new Vue({
     el: '#app'
 });
+
+const x = 12345;
+
+if (x) {
+    /**
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     */
+}
